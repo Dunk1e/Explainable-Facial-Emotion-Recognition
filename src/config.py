f691aaa820a2f2535715525255948ab10abea8fd
Josia Vahutinskij <josia@Mac.fritz.box>
2026-01-20 08:02:43 +0100

    changed config so run training.py works

--- src/config.py
+++ src/config.py
@@ -3,8 +3,13 @@ from pathlib import Path
 
 @dataclass
 class Config:
-    project_root: Path = Path(__file__).resolve().parent
+    project_root: Path = Path(__file__).resolve().parents[1]
 
     train_dir: Path = project_root / "train"
     val_dir: Path = project_root / "validation"
     test_dir: Path = project_root / "test"
+
+    batch_size: int = 64
+    num_workers: int = 2
+    lr: float = 3e-4
+    epochs: int = 30
